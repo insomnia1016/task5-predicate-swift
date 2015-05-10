ae0bca9b3ecc7d0d9d381ed878df032c2b6c4d74
chenjianwu <chenjianwu@chenjianwudeMacBook-Pro.local>
2015-05-10 09:28:32 +0800

    Initial Commit

---
+++ task5 predicate swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  task5 predicate swift
+//
+//  Created by chenjianwu on 15-5-10.
+//  Copyright (c) 2015年 陈建武工作室. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
